de2b3b51a74a79c18fa58e8cc90daf6d71e4d598
Diana De Leon <dianadeleon2406@gmail.com>
2022-09-10 16:33:39 -0600

    Modelos

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import EstudiantePublica,EstudianteAutoriza,Articulos,Publicaciones,Comentarios
 # Register your models here.
+
+admin.site.register(EstudiantePublica)
+admin.site.register(EstudianteAutoriza)
+admin.site.register(Articulos)
+admin.site.register(Publicaciones)
+admin.site.register(Comentarios)
--- apps/home/models.py
+++ apps/home/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+
+class EstudiantePublica(models.Model):
+    carnet = models.CharField(max_length=5)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s %s' % (self.nombre,self.apellido)
+
+
+class EstudianteAutoriza(models.Model):
+    carnet = models.CharField(max_length=5)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s %s' % (self.nombre,self.apellido)
+
+class Articulos(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=500)
+    autoriza = models.ForeignKey(EstudianteAutoriza,on_delete=models.CASCADE)
+    creacion = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return '%s' % (self.titulo)
+
+
+class Publicaciones(models.Model):
+    articulo = models.ForeignKey(Articulos,on_delete=models.CASCADE)
+    publica = models.ForeignKey(EstudiantePublica,on_delete=models.CASCADE)
+    creacion = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return '%s %s' % (self.articulo,self.publica)
+
+class Comentarios(models.Model):
+    articulo = models.ManyToManyField(Articulos)
+    comentario = models.CharField(max_length=500)
+    comenta = models.ForeignKey(EstudiantePublica,on_delete=models.CASCADE) 
+    creacion = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return '%s %s' % (self.articulo,self.comentario)
